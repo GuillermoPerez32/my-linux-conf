a2c6f3e810dcf5f0ebaddd91029f62d66409fc5f
GuillermoPerez32 <luisguillermo.rodriguez32@gmail.com>
2022-02-28 17:53:54 -0500

    First add

---
+++ cntlm/.bashrc
@@ -0,0 +1,3 @@
+# at the end
+export http_proxy="http://127.0.0.1:3128"
+export https_proxy="http://127.0.0.1:3128"
---
+++ cntlm/commands.sh
@@ -0,0 +1,4 @@
+sudo service cntlm start;
+sudo service cntlm start;
+sudo nano /etc/enviroments;
+sudo nano /etc/cntlm.conf;
